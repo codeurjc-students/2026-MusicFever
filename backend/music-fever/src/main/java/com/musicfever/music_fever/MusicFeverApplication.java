bd2029ecb6a1e21add40532fc012d8c73c5d60d5
armin <armingarc@yahoo.com>
2026-09-12 18:32:40 +0200

    setup: create server-side project. Closes #14.

---
+++ backend/music-fever/src/main/java/com/musicfever/music_fever/MusicFeverApplication.java
@@ -0,0 +1,13 @@
+package com.musicfever.music_fever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicFeverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicFeverApplication.class, args);
+	}
+
+}
---
+++ backend/music-fever/src/test/java/com/musicfever/music_fever/MusicFeverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musicfever.music_fever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicFeverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
